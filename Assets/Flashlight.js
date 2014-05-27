a344deca7cdb339ab56424fea5678709a9b2b8d2
Garretthogan <Garretthogan18@gmail.com>
2014-05-27 10:27:41 -0500

    All the AI Stuff
    Everything needed for enemy AI is here

---
+++ Assets/Flashlight.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+
+private var StateManager : AIStateManager;
+
+function OnTriggerEnter(other : Collider)
+{
+	if(other.gameObject.CompareTag("Player"))
+	{
+		StateManager.currentState = EnemyStates.PURSUIT;
+	}
+}
+
+function Start () {
+
+	StateManager = transform.parent.gameObject.GetComponent("AIStateManager");
+}
+
+function Update () {
+
+}
---
+++ Assets/Hearing.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+
+private var StateManager : AIStateManager;
+
+function OnTriggerEnter(other : Collider)
+{
+	if(other.gameObject.CompareTag("Player"))
+	{
+		StateManager.playerLastLocation.transform.position = other.gameObject.transform.position;
+		StateManager.currentState = EnemyStates.INVESTIGATE;
+	}
+}
+
+function Start () {
+
+	StateManager = transform.parent.gameObject.GetComponent("AIStateManager");
+}
+
+function Update () {
+
+}
