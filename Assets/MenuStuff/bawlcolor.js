408167b22c996fb8a6529cfa767a178f769ee2f7
Rundas42 <therisingsun777@yahoo.com>
2014-06-03 12:12:40 -0500

    MainMenu

---
+++ Assets/MenuStuff/bawlcolor.js
@@ -0,0 +1,28 @@
+﻿#pragma strict
+
+function Start () 
+{
+	renderer.enabled = false;
+	transform.FindChild("TopOfExcl").renderer.enabled = false;
+}
+
+
+function Update () 
+{
+
+	
+	if(Input.GetKeyDown(KeyCode.R))
+	{
+	renderer.enabled = true;
+	
+	 	gameObject.renderer.material.color = Color.red;
+	 	//transform.GetChild(0).renderer.material.color = Color.red;
+	 	transform.FindChild("TopOfExcl").renderer.material.color = Color.red;
+	 	transform.FindChild("TopOfExcl").renderer.enabled = true;
+	}
+
+
+		
+
+		//gameObject.renderer.material.color.a = 1.0f;
+}
